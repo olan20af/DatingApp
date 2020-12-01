35412257e16411d71c89008aa684b900722ea19b
unknown <Oliver Andreasen>
2020-12-01 09:08:15 +0100

    first commit

---
+++ models/user.js
@@ -0,0 +1,15 @@
+module.exports = class User {
+    constructor(user_id, user_email, user_name, user_gender, user_age, user_description, user_work, user_school) {
+
+        this.user_id = user_id;
+        this.user_email = user_email;
+        this.user_name = user_name;
+        this.user_gender = user_gender; 
+        this.user_age = user_age;
+        this.user_description = user_description;
+        this.user_work = user_work;
+        this.user_school = user_school;
+
+    }
+}
+
